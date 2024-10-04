1975852ef28eac98e12d06b135de31f55a99f9b0
Arty <kirya26763@mail.ru>
2024-10-04 21:33:09 +0700

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        Author authorBook = new Author("Уильям", "Шекспир");
+        Book book = new Book(1623, "Буря", authorBook);
+        System.out.println(book);
+        Author authorBook1 = new Author("Фёдор", "Достоевски");
+        Book book1 = new Book(1866, "Преступление и наказание", authorBook1);
+        System.out.println(book1);
     }
 }
